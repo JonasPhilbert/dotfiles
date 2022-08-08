74d9814cd9774a3fd3a522ea443ab09f1beebcee
JonasPhilbert <jonas@priestyard.dk>
2022-08-08 08:40:42 +0200

    Use fugitive indicator for lualine.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -161,6 +161,9 @@ require('lualine').setup({
     theme = 'onedark',
     component_separators = {},
     section_separators = {},
+    sections = {
+      lualine_a = {'FugitiveHead'},
+    },
   }
 })
 
