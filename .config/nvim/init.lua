39395bef7b70b85761da3ee32a478a600408c8fa
Jonas Philbert <6285509+JonasPhilbert@users.noreply.github.com>
2022-11-04 11:10:52 +0100

    Reintroduced Rag command.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -125,12 +125,11 @@ require('packer').startup(function(use)
   }
   vim.g.fzf_layout = { window = { width = 1, height = 1 } } -- FZF window fill entire screen.
   vim.cmd("command! -bang -nargs=* Ag call fzf#vim#ag(<q-args>, fzf#vim#with_preview({'options': '--delimiter : --nth 4..'}), <bang>0)") -- https://github.com/junegunn/fzf.vim/issues/346#issuecomment-655446292
-  vim.cmd("command! -bang -nargs=* Rag call fzf#vim#ag(<q-args>, fzf#vim#with_preview({}), <bang>0)")
+  vim.cmd("command! -bang -nargs=+ -complete=dir Rag call fzf#vim#ag_raw(<q-args>, {'options': '--delimiter : --nth 4..'}, <bang>0)") -- Use <:Rag 'query' path> to search for ag matches in specific folder
   vim.keymap.set('n', '<leader><space>', ':Buffers<CR>', nore_silent)
   vim.keymap.set('n', '<leader>f', ':GFiles<CR>', nore_silent)
   vim.keymap.set('n', '<leader>F', ':Ag<CR>', nore_silent)
   vim.keymap.set('v', '<leader>F', 'y:Ag <c-r>0<CR>', nore_silent)
-  -- command! -bang -nargs=+ -complete=dir Rag call fzf#vim#ag_raw(<q-args>, {'options': '--delimiter : --nth 4..'}, <bang>0) -- Use <:Rag 'query' path> to search for ag matches in specific folder
 
   -- Change surrounding delimiter with cs<d><d> (eg. cs"{ )
   use 'tpope/vim-surround'
