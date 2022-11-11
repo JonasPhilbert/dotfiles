a4fcb0f8c59eb95716f127bed34a4f3ed8c3032a
Jonas Philbert <6285509+JonasPhilbert@users.noreply.github.com>
2022-11-11 13:50:44 +0100

    Use brackets for command, for clearity.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -40,7 +40,7 @@ local nore_silent = { noremap = true, silent = true }
 vim.cmd([[command! Rm :call system('zsh -c "trash %"') | bd]])
 
 -- Command to rename current file: :Rename new_name.foo
-vim.cmd("command! -nargs=1 Rename saveas <args> | call delete(expand('#')) | bd #")
+vim.cmd([[command! -nargs=1 Rename saveas <args> | call delete(expand('#')) | bd #]])
 
 -- Switch to last used buffer using backspace.
 vim.keymap.set('n', '<BS>', '<C-^><CR>', nore_silent)
