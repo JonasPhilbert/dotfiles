83bab7fc92eb31838413228108655fc0e731b3b9
Jonas Philbert <6285509+JonasPhilbert@users.noreply.github.com>
2023-01-25 15:23:26 +0100

    Install node in setup + Start fish once setup is done.

--- setup.sh
+++ setup.sh
@@ -14,6 +14,7 @@ brew install git
 # Clone dotfiles repo to home dir
 git clone https://github.com/JonasPhilbert/dotfiles.git .
 
+# Install homebrew packages
 brew install fish
 brew install fisher
 brew install neovim
@@ -21,10 +22,18 @@ brew install tmux
 brew install ripgrep
 brew install fzf
 
+# Install homebrew casks
 brew install --cask alacritty
+
+# Install latest node version
+fish -c "nvm install latest"
+
+clear
 
 echo 'Setup complete. Further steps to consider:'
 echo '  1) Set default shell to fish using chsh.'
 echo '  2) Start nvim and run :PackerSync to install packages and initialize.'
 echo '  3) Move/copy .secrets from another machine.'
 echo '  4) Download and install other environment-specific packages.'
+
+exec fish
