219e14ba1428272cffa70f017c21400f541c4431
Jonas Philbert <6285509+JonasPhilbert@users.noreply.github.com>
2024-03-20 16:23:47 +0100

    Updated comment.

--- .nixos/configuration.nix
+++ .nixos/configuration.nix
@@ -32,7 +32,7 @@
     LC_TIME = "da_DK.UTF-8";
   };
 
-  # Enable the KDE (Plasma) Desktop Environment.
+  # Enable the GNOME Desktop Environment.
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
