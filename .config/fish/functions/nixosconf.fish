de74f70973556141f1a817a52b9b9f79c21b861d
Jonas Philbert <6285509+JonasPhilbert@users.noreply.github.com>
2024-03-21 14:34:59 +0100

    Updated nixosconf function to abort if configuration.nix has no changes done to it.

--- .config/fish/functions/nixosconf.fish
+++ .config/fish/functions/nixosconf.fish
@@ -2,11 +2,15 @@ function nixosconf --description 'Edits the current NixOS configuration, commits
   set old_dir $PWD
   cd ~/.nixos
   nvim configuration.nix
-  git add configuration.nix
-  git commit
-  nixos-generate-config --dir .
-  git add -f hardware-configuration.nix
-  sudo nixos-rebuild switch --flake .#nixos
-  git rm -f hardware-configuration.nix
+  if test -n (git diff -- configuration.nix);
+    echo "No changes to configuration.nix detected. Aborting."
+  else;
+    git add configuration.nix
+    git commit
+    nixos-generate-config --dir .
+    git add -f hardware-configuration.nix
+    sudo nixos-rebuild switch --flake .#nixos
+    git rm -f hardware-configuration.nix
+  end
   cd $old_dir
 end
