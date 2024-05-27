de185f92bdadb94fa032cb956368b91ea21546e9
Jonas Philbert <6285509+JonasPhilbert@users.noreply.github.com>
2024-05-27 11:44:13 +0000

    Added nix shell output after commands to indicate if we're in a nix shell.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,8 +10,16 @@ alias rc="$EDITOR ~/.config/fish/config.fish"
 alias tmuxdev="tmux split-window -h && tmux split-window && tmux resize-pane -R 30 && tmux select-pane -L && tmux rename-window DEVELOPMENT"
 alias tmuxops="tmux split-window -h && tmux select-pane -L && tmux rename-window OPS"
 alias notes="$EDITOR ~/NOTES.md"
-alias rm="echo You should probably use 'trash' instead. To override, use full bin path: '/bin/rm'."
+alias rails="bundle exec rails"
+# alias rm="echo You should probably use 'trash' instead. To override, use full bin path: '/bin/rm'."
 
 # Extended Config Sources
 
 # source ~/.config/fish/flightlogger.fish
+
+# Nix shell status in prompt.
+function postexec_nixshell --on-event fish_postexec
+  if test -n "$IN_NIX_SHELL"
+    echo (set_color --italic cyan) "❄️ In Nix shell: " (set_color --bold red) $IN_NIX_SHELL
+  end
+end
