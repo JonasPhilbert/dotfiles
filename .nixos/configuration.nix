d6c0766fffea41333e0b0c86abb1231760d05aac
Jonas Philbert <6285509+JonasPhilbert@users.noreply.github.com>
2024-05-27 11:04:09 +0200

    Added auth config to psql.

--- .nixos/configuration.nix
+++ .nixos/configuration.nix
@@ -172,5 +172,9 @@
   # PostgreSQL Config
   services.postgresql = {
     enable = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser  auth-method
+      local all       all     trust
+    '';
   };
 }
