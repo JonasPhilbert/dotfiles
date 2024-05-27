e20103628780a8d27ee4ffb94b241cfb19c59ace
Jonas Philbert <6285509+JonasPhilbert@users.noreply.github.com>
2024-05-27 11:20:22 +0200

    Reverting. Something's wrong.

--- .nixos/configuration.nix
+++ .nixos/configuration.nix
@@ -172,9 +172,5 @@
   # PostgreSQL Config
   services.postgresql = {
     enable = true;
-    authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
-      local all       all     trust
-    '';
   };
 }
