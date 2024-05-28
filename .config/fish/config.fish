c0a1846d0d0728ca961a17b92577ea1c45b19030
Jonas Philbert <6285509+JonasPhilbert@users.noreply.github.com>
2024-05-29 00:21:50 +0200

    Cleanup of fish shell. Removed old plugins for a more pure experience.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,22 @@
+# Load all statements from the .secrets file, if it exists.
 if test -e ~/.secrets
   source ~/.secrets
 end
 
+# Set nvim as the default editor vis the EDITOR envvar.
 set -gx EDITOR nvim
 
 # Aliases
-
 alias rc="$EDITOR ~/.config/fish/config.fish"
 alias tmuxdev="tmux split-window -h && tmux split-window && tmux resize-pane -R 30 && tmux select-pane -L && tmux rename-window DEVELOPMENT"
 alias tmuxops="tmux split-window -h && tmux select-pane -L && tmux rename-window OPS"
 alias notes="$EDITOR ~/NOTES.md"
-alias rails="bundle exec rails"
 # alias rm="echo You should probably use 'trash' instead. To override, use full bin path: '/bin/rm'."
 
 # Extended Config Sources
 
 # source ~/.config/fish/flightlogger.fish
+source ~/.config/fish/rails.fish
 
 # Nix shell status in prompt.
 function postexec_nixshell --on-event fish_postexec
