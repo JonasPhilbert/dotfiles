c5e30ab3d02d34b2f511bc93a4c3fc9e3fbf810c
Jonas Philbert <6285509+JonasPhilbert@users.noreply.github.com>
2024-05-29 09:46:41 +0200

    Cleaned up and upgraded nixosconf and nixosbuild commands. Most importantly, nixosconf uses nixosbuild internally, removing duplicate code.

--- .config/fish/functions/nixosbuild.fish
+++ .config/fish/functions/nixosbuild.fish
@@ -1,9 +1,11 @@
 function nixosbuild --description 'Rebuilds and switches NixOS.'
   set old_dir $PWD
   cd ~/.nixos
-  nixos-generate-config --dir .
-  git add -f hardware-configuration.nix
-  sudo nixos-rebuild switch --flake .#nixos
-  git rm -f hardware-configuration.nix
+  nixos-generate-config --dir . # Generate hardware configuration to PWD.
+  git add -f hardware-configuration.nix # Nix will not consider files not added to git in the build, so even though we don't wish to commit the hardware config, we must add it here for the build.
+  sudo nixos-rebuild switch --flake .#nixos # The actual build.
+  set success $status
+  git rm -f hardware-configuration.nix # Remove hardware config from git. We don't actually want to commit it.
   cd $old_dir
+  return $success
 end
--- .config/fish/functions/nixosconf.fish
+++ .config/fish/functions/nixosconf.fish
@@ -1,19 +1,18 @@
-function nixosconf --description 'Edits the current NixOS configuration, commits the changes and finally rebuilds and switches NixOS.'
+function nixosconf --description 'Edits the current NixOS configuration, rebuilds and switches NixOS and, if successful, commits the changes.'
   set old_dir $PWD
   cd ~/.nixos
   nvim configuration.nix
   if test -n "$(git diff -- configuration.nix)";
     git add configuration.nix
-    nixos-generate-config --dir .
-    git add -f hardware-configuration.nix
-    sudo nixos-rebuild switch --flake .#nixos
+    nixosbuild
     set success $status
-    git rm -f hardware-configuration.nix
     if test $success -eq 0;
       git commit
+    else
+      echo (set_color "red")"NixOS build failed."
     end
   else;
-    echo "No changes to configuration.nix detected. Aborting."
+    echo (set_color "magenta")"No changes to configuration.nix detected. Aborting."
   end
   cd $old_dir
 end
